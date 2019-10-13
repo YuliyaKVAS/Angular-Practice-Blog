c7944d47d83e41fb15c8b4148d579da6413d27bb
Yulia Kusakina <yulia_kusakina@epam.com>
2019-10-13 21:46:25 +0300

    github search bugfix

--- blog/src/app/admin/github-search-page/github-search-page.component.ts
+++ blog/src/app/admin/github-search-page/github-search-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FormControl } from "@angular/forms";
 import {
   catchError,
@@ -19,14 +19,15 @@ import { GithubService } from "../../shared/github.service";
   styleUrls: ['./github-search-page.component.scss']
 })
 
-export class GithubSearchPageComponent {
-
+export class GithubSearchPageComponent implements OnInit{
   users = [];
   isLoading = false;
 
   searchQuery = new FormControl();
 
-  constructor(private githubService: GithubService) {
+  constructor(private githubService: GithubService) {}
+
+  ngOnInit() {
     this.searchQuery.valueChanges
       .pipe(
         debounceTime(1000),
@@ -38,9 +39,9 @@ export class GithubSearchPageComponent {
         ),
         map(value => value['items'])
       ).subscribe(value => {
-        this.isLoading = true;
-        this.users = value;
-        this.isLoading = false;
+      this.isLoading = true;
+      this.users = value;
+      this.isLoading = false;
     })
   }
 }
